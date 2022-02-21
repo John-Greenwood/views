779c8f88dda0efa0489d132b2df84319f963be9c
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2022-02-21 18:13:33 +0300

    no message

--- Sources/Views/Indicator/RefreshControl.swift
+++ Sources/Views/Indicator/RefreshControl.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 public class RefreshControl: UIRefreshControl {
-    var indicator: Indicator
+    public var indicator: Indicator?
     private var observing: Bool = false
     private var scrollInsets: UIEdgeInsets = .zero
     private var previousOffset: CGPoint = .zero
@@ -9,12 +9,12 @@ public class RefreshControl: UIRefreshControl {
     
     public override func beginRefreshing() {
         super.beginRefreshing()
-        indicator.start()
+        indicator?.start()
     }
     
     public override func endRefreshing() {
         super.endRefreshing()
-        indicator.stop()
+        indicator?.stop()
         previuslyOffset = .zero
     }
     
@@ -23,24 +23,25 @@ public class RefreshControl: UIRefreshControl {
             removeObserving()
         }
         didSet {
-            if let scrollView = scrollView {
+            if let scrollView = scrollView, let indicator = indicator {
                 scrollInsets = scrollView.contentInset
                 addObserving()
                 bringSubviewToFront(indicator)
             }
         }
     }
-    
-    override init() {
-        indicator = Indicator(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
-        super.init(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+}
+
+public extension RefreshControl {
+    convenience init(size: CGSize = .zero) {
+        self.init(frame: .init(origin: .zero, size: size))
+        let indicator = Indicator(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
         tintColor = .clear
         addSubview(indicator)
         indicator.center.x = UIScreen.main.bounds.width / 2
         indicator.center.y = 30
+        self.indicator = indicator
     }
-    
-    required public init?(coder: NSCoder) { nil }
 }
 
 extension RefreshControl {
@@ -78,9 +79,9 @@ extension RefreshControl {
             if previuslyOffset <= progress { previuslyOffset = progress }
             UIView.animate(withDuration: TimeInterval(0.01)) {
                 //self.indicator.frame.origin.y = -offset - (refreshViewHeight / 2)
-                self.indicator.alpha = -offset.map(min1: 38, max1: 150, min2: 0, max2: 1)
+                self.indicator?.alpha = -offset.map(min1: 38, max1: 150, min2: 0, max2: 1)
             }
-            indicator.rotate(value: progress)
+            indicator?.rotate(value: progress)
         } else {
             super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
         }
